6a73403be41a7b304b089989ac0bde38bed1b228
Hansjurgen Hinkins Herd <32222451+hans2herd@users.noreply.github.com>
2019-03-22 11:04:38 -0600

    Create kata41.js

---
+++ kata41.js
@@ -0,0 +1,47 @@
+// 41: array - entries
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+//          ORIGINAL
+// describe('`[].entries()` returns an iterator object with all entries', function() {
+//     it('returns key+value for each element', function() {
+//       const arr = ['a', 'b', 'c'];
+//       const entriesAsArray = Array.from(arr.___());
+//       assert.deepEqual(entriesAsArray, [[0,"a"], [1,"b"], [2,"c"]]);
+//     });
+//     it('empty elements contain the value `undefined`', function() {
+//       const arr = ['one'];
+//       arr[2] = 'three';
+//       const secondValue = arr.entries();
+//       assert.deepEqual(secondValue, [1, void 0]);
+//     });
+//     describe('returns an iterable', function() {
+//       it('has `next()` to iterate', function() {
+//         const arr = ['one'];
+//         const value = arr;
+//         assert.deepEqual(value, [0, 'one']);
+//       });
+//     });
+//   });
+
+//          FIXED
+describe('`[].entries()` returns an iterator object with all entries', function() {
+    it('returns key+value for each element', function() {
+      const arr = ['a', 'b', 'c'];
+      const entriesAsArray = Array.from(arr.entries());
+      assert.deepEqual(entriesAsArray, [[0,"a"], [1,"b"], [2,"c"]]);
+    });
+    it('empty elements contain the value `undefined`', function() {
+      const arr = ['one'];
+      arr[2] = 'three';
+      const secondValue = Array.from(arr.entries())[1];
+      assert.deepEqual(secondValue, [1, void 0]);
+    });
+    describe('returns an iterable', function() {
+      it('has `next()` to iterate', function() {
+        const arr = ['one'];
+        const value = Array.from(arr.entries())[0];
+        assert.deepEqual(value, [0, 'one']);
+      });
+    });
+  });
