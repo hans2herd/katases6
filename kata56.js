13451335dda608584d4f3afddc2a8e195d670218
Hansjurgen Hinkins Herd <32222451+hans2herd@users.noreply.github.com>
2019-04-05 11:50:44 -0600

    Update kata56.js

--- kata56.js
+++ kata56.js
@@ -32,30 +32,31 @@
 
 //          FIXED
 describe('Pass a function to a generator', () => {
-    it('the generator can receive a function as a value', function() {
-      let fn = function() {};
-      function* generatorFunction() { yield
-        assert.equal(yield null, fn); // remember, don't touch this line
-      }
-      let iterator = generatorFunction();
-      iterator.next();
-      iterator.next(fn);
-    });
-    it('pass a function to the iterator, which calls it', function() {
-      function* generatorFunction() {
-        yield (yield 1)(yield 2);
-      }
-      var iterator = generatorFunction();
-      var iteratedOver = [iterator.next().value, iterator.next().value];
-      assert.deepEqual([1, 2], iteratedOver);
-    });
-    it('nesting yielded function calls', function() {
-      function* generatorFunction() {
+  it('the generator can receive a function as a value', function() {
+    let fn = function() {};
+    function* generatorFunction(fn) {
+      assert.equal(yield null, fn); // remember, don't touch this line
+    }
+    let iterator = generatorFunction();
+    iterator.next();
+    iterator.next();
+  });
+  it('pass a function to the iterator, which calls it', function() {
+    function* generatorFunction() {
+      yield (yield 1)();
+    }
+    var iterator = generatorFunction();
+    var iteratedOver = [iterator.next().value, iterator.next(() => 2).value];
+    assert.deepEqual([1, 2], iteratedOver);
+  });
+  it('nesting yielded function calls', function() {
+    function* generatorFunction() {
         yield (yield (yield 1)());
       }
-      
-      var iteratedOver = [iterator.next().value, iterator.next(fn).value,iterator.next(3).value]
+      var iterator = generatorFunction()
+      var iteratedOver = [iterator.next().value, iterator.next(() => 2).value,iterator.next(3).value]
       assert.deepEqual([1, 2, 3], iteratedOver);
-    });
   });
+});
+     
   
